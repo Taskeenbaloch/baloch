b64c36ab6f045118bac8e32349c197089eeee5c5
Taskeenbaloch <113247924+Taskeenbaloch@users.noreply.github.com>
2022-10-04 16:47:32 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,7 +37,7 @@ global.APIKeys = {
 
 //aumto functioner
 global.autoTyping = true //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = trur //auto reading in gc and pm (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = true //auto reading in gc (true to on, false to off)
 global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
